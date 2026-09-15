2c5dd94419b6546a44107f254c8b93a2e582e20f
ahmed <ahalfasean@gmail.com>
2026-09-15 17:16:35 +0100

    Implement Caesar cipher with input validation
    Added input validation for command-line argument and key.

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -1,1 +1,54 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(int argc, string argv[])
+{
+    // Cheeck that there is exactly one command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    // Check that erevy character in the key is a digit
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+    // Convert the key from a string to an integer
+    int key = atoi(argv[1]);
+    key = key % 26;
+
+    // Get plaintext from the user
+    string plaintext = get_string("plaintext:  ");
+
+    printf("ciphertext: ");
+
+    // Encrypt each character
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isupper(plaintext[i]))
+        {
+            printf("%c", (plaintext[i] - 'A' + key) % 26 + 'A');
+        }
+        else if (islower(plaintext[i]))
+        {
+            printf("%c", (plaintext[i] - 'a' + key) % 26 + 'a');
+        }
+        else
+        {
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
