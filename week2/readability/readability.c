2cdf83353b91300220bbe671b085f4bb2555d4a9
ahmed <ahalfasean@gmail.com>
2026-09-15 17:14:44 +0100

    Implement readability score calculation
    Count letters, words, and sentences in user input text to compute the Coleman-Liau index and print the corresponding grade level.

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,1 +1,62 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
+int main(void)
+{
+    // Promt the user for some text
+    string text = get_string("text: ");
+
+    // Count the number of letters,words, and sentences
+    int letters = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+
+    int words = 1;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+    }
+
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    // Cumpute the Colmeman-Liau index
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    // Print the grade level
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
